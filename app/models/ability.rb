c1c41e45efb13a5798a0e0b81de1ff2ea7284da1
mennogodeke <mgodeke1994@gmail.com>
2016-11-09 12:47:14 +0100

    scaffolded projects

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,6 +14,7 @@ class Ability
       can :read, Article
       can :read, Skill
       can :read, Activety
+      can :read, Project
     end
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109105737) do
+ActiveRecord::Schema.define(version: 20161109113122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20161109105737) do
     t.string   "user"
     t.integer  "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "skills", force: :cascade do |t|
